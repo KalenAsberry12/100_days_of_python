a97a19d40398a8d2a94ba3178889eb7117697f21
Kalen Asberry <62851371+KalenAsberry12@users.noreply.github.com>
2023-02-16 11:13:32 -0500

    Add files via upload

---
+++ treasure-island.py
@@ -0,0 +1,57 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("Which direction do you want to go Left or Right? ")
+instruction = input("Do you want to swim or wait ? ")
+which_door = input("Which door color do you want to open R, B or Y? ")
+
+
+
+if ((direction == "Left") and (instruction == "wait") and (which_door == "Y")):
+  print("You Win!")
+elif ((direction == "Left") and (instruction == "wait") and (which_door == "B")):
+  print("Eaten by beasts. Game Over.")
+elif ((direction == "Left") and (instruction == "wait") and (which_door == "R")):
+  print("Burned by fire. Game Over.")
+elif (direction == "Left") and (instruction == "swim"):
+  print("Attacked by trout. Game Over.")
+elif (direction == "Right" and (instruction != "swim") or (instruction != "wait") or (which_door != "R") or (which_door != "Y") or (which_door != "B")):
+  print("Fall into a hole. Game Over.")
+else:
+    print("Game Over")
+
+
+
+
+
+
+
+
+
+   
+  
